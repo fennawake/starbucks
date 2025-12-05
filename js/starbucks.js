0ea1d3e603417090889606485cfb5d5cae7a8eea
Luiz Fernando Souza <fenna.wake@gmail.com>
2025-12-04 22:35:35 -0300

    feat: Implement first html and css codes.

---
+++ js/starbucks.js
@@ -0,0 +1,6 @@
+// Change Starbucks cup image on button click and change background color
+let  imagem = document.querySelector('.imagem-copo');
+
+function changeImage(img) {
+    imagem.src = img;
+}
